73fef37eca8251ee9b66c6ec40a45c265d138960
adevotus <adevoweb@gmail.com>
2023-02-18 12:05:34 +0300

    new updates

--- resources/views/admin/whats_new/news.blade.php
+++ resources/views/admin/whats_new/news.blade.php
@@ -12,9 +12,7 @@
             <div class="navbar-custom">
             <div class="container-fluid">
            <ul class="list-unstyled topnav-menu float-end mb-0">
-
           
-    
     
             <li class="dropdown notification-list topbar-dropdown">
                 <a class="nav-link dropdown-toggle nav-user me-0 waves-effect waves-light" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
@@ -118,61 +116,204 @@
 
                     <!-- Start Content-->
                     <div class="container-fluid">
-                        
+    
                         <!-- start page title -->
                         <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box">
+                                   
                                     <h4 class="page-title">News</h4>
                                 </div>
                             </div>
-                        </div>     
-                        <!-- end page title --> 
-
-                        <div class="row">
-                            <div class="col-12">
-                                <button class="btn" ><i class="fa fa-plus-circle" aria-hidden="true"></i></button>
-                            </div>
-                        </div> 
-                        
-                        <div class="form-outline mt-2 mb-2">
-                            <input type="search" class="form-control" id="datatable-search-input" placeholder="Filter">
-                          </div>
-
-                        <div class="row">
-                            <div class="col-12">
-                                <table class="table table-striped-columns mt-4">
-                                    <thead>
-                                      <tr class="table-info">
-                                        <th scope="col">SN</th>
-                                        <th scope="col">Image</th>
-                                        <th scope="col">Title</th>
-                                        <th scope="col">Description</th>
-                                        <th scope="col">Actions</th>
-                                      </tr>
-                                    </thead>
-                                    <tbody>
-                                      <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                        <td>@mdo</td>
-                                      </tr>
-                                      <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                        <td>@mdo</td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                            </div>
                         </div>
-                        <!-- container -->
+                        <!-- end page title -->
+    
+    
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="row justify-content-between mb-2">
+                                            <div class="col-auto">
+                                                <form>
+                                                    <div class="mb-2">
+                                                        <label for="inputPassword2" class="visually-hidden">Search</label>
+                                                        <input type="search" class="form-control" id="inputPassword2" placeholder="Search...">
+                                                    </div>
+                                                </form>
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <div class="text-sm-end">
+                                                    {{-- <button type="button" class="btn btn-danger waves-effect waves-light mb-2" data-bs-toggle="modal" data-bs-target="#custom-modal">Add Contact</button> --}}
+                                                    <Span class="bg-info p-2"  style="color:#fff" data-bs-toggle="modal" data-bs-target="#custom-modal"><a href="#" data-bs-toggle="modal" data-bs-target="#custom-modal" style="color:#fff">Add news</a> </Span>
+                                                </div>
+                                            </div>
+                                          
+                                        </div>
+    
+                                        <div class="table-responsive">
+                                            <table class="table table-centered table-nowrap table-hover mb-0">
+                                                <thead>
+                                                    <tr>
+                                                        <th>sn</th>
+                                                        <th>title</th>
+                                                        <th>Image</th>
+                                                        <th>Discription</th>
+                                                        <th>Created Date</th>
+                                                        <th style="width: 82px;">Action</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td class="table-user">
+                                                            <img src="../assets/images/users/user-4.jpg" alt="table-user" class="me-2 rounded-circle">
+                                                            <a href="javascript:void(0);" class="text-body fw-semibold">Paul J. Friend</a>
+                                                        </td>
+                                                        <td>
+                                                            937-330-1634
+                                                        </td>
+                                                        <td>
+                                                            pauljfrnd@jourrapide.com
+                                                        </td>
+                                                        <td>
+                                                            Vine Corporation
+                                                        </td>
+                                                        <td>
+                                                            07/07/2018
+                                                        </td>
+                                                        <td>
+                                                            <a href="javascript:void(0);" class="action-icon"> <i class="mdi mdi-square-edit-outline"></i></a>
+                                                            <a href="javascript:void(0);" class="action-icon"> <i class="mdi mdi-delete"></i></a>
+                                                        </td>
+                                                    </tr>
+    
+                                                    <tr>
+                                                        <td class="table-user">
+                                                            <img src="../assets/images/users/user-3.jpg" alt="table-user" class="me-2 rounded-circle">
+                                                            <a href="javascript:void(0);" class="text-body fw-semibold">Bryan J. Luellen</a>
+                                                        </td>
+                                                        <td>
+                                                            215-302-3376
+                                                        </td>
+                                                        <td>
+                                                            bryuellen@dayrep.com
+                                                        </td>
+                                                        <td>
+                                                            Blue Motors
+                                                        </td>
+                                                        <td>
+                                                            09/12/2018
+                                                        </td>
+                                                        <td>
+                                                            <a href="javascript:void(0);" class="action-icon"> <i class="mdi mdi-square-edit-outline"></i></a>
+                                                            <a href="javascript:void(0);" class="action-icon"> <i class="mdi mdi-delete"></i></a>
+                                                        </td>
+                                                    </tr>
+    
+                                                    <tr>
+                                                        <td class="table-user">
+                                                            <img src="../assets/images/users/user-3.jpg" alt="table-user" class="me-2 rounded-circle">
+                                                            <a href="javascript:void(0);" class="text-body fw-semibold">Kathryn S. Collier</a>
+                                                        </td>
+                                                        <td>
+                                                            828-216-2190
+                                                        </td>
+                                                        <td>
+                                                            collier@jourrapide.com
+                                                        </td>
+                                                        <td>
+                                                            Arcanetworks
+                                                        </td>
+                                                        <td>
+                                                            06/30/2018
+                                                        </td>
+                                                        <td>
+                                                            <a href="javascript:void(0);" class="action-icon"> <i class="mdi mdi-square-edit-outline"></i></a>
+                                                            <a href="javascript:void(0);" class="action-icon"> <i class="mdi mdi-delete"></i></a>
+                                                        </td>
+                                                    </tr>
+    
+                                                   
+    
+                                                   
+    
+                                                </tbody>
+                                            </table>
+                                        </div>
+    
+                                        <ul class="pagination pagination-rounded justify-content-end mb-0 mt-2">
+                                            <li class="page-item">
+                                                <a class="page-link" href="javascript: void(0);" aria-label="Previous">
+                                                    <span aria-hidden="true">«</span>
+                                                    <span class="visually-hidden">Previous</span>
+                                                </a>
+                                            </li>
+                                            <li class="page-item active"><a class="page-link" href="javascript: void(0);">1</a></li>
+                                            <li class="page-item"><a class="page-link" href="javascript: void(0);">2</a></li>
+                                            <li class="page-item"><a class="page-link" href="javascript: void(0);">3</a></li>
+                                            <li class="page-item"><a class="page-link" href="javascript: void(0);">4</a></li>
+                                            <li class="page-item"><a class="page-link" href="javascript: void(0);">5</a></li>
+                                            <li class="page-item">
+                                                <a class="page-link" href="javascript: void(0);" aria-label="Next">
+                                                    <span aria-hidden="true">»</span>
+                                                    <span class="visually-hidden">Next</span>
+                                                </a>
+                                            </li>
+                                        </ul>
+    
+                                    </div>
+                                    <!-- end card-body-->
+                                </div>
+                                <!-- end card-->
+                            </div>
+                            <!-- end col -->
+    
+                        </div>
+                        <!-- end row -->
+    
+                    </div>
+                    <!-- container -->
+    
+                </div>
+                <!-- content -->
+                  <!-- Modal -->
+    <div class="modal fade" id="custom-modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header bg-light">
+                    <h4 class="modal-title" id="myCenterModalLabel">Add News</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body p-4">
+                    <form>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Title</label>
+                            <input type="text" class="form-control" id="name" placeholder="Enter name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Image</label>
+                            <input type="file" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="position" class="form-label">Initial description</label>
+                        <textarea name="initial description" id="" cols="12" rows="3" class="form-control"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="company" class="form-label">Description</label>
+                            <textarea name="news descripiton" id="" cols="30" rows="10" class="form-control"></textarea>
+                        </div>
 
-                </div> <!-- content -->
+                        <div class="text-end">
+                            <button type="submit" class="bg-info p-2">Publish </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
 
                 <!-- Footer Start -->
                 <footer class="footer">
