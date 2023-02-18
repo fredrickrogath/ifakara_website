4c7b18971bd8cc1f0e8504eaed3f4bab22e706df
Kamasho <kamashogodfrey997@gmail.com>
2023-02-18 13:59:26 +0300

    edit events

--- resources/views/admin/whats_new/events.blade.php
+++ resources/views/admin/whats_new/events.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
     @include('layouts.admin.head')
 
@@ -203,4 +203,345 @@
         <script src="{{asset('admin/assets/js/app.min.js')}}"></script>
         
     </body>
+</html> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+@include('layouts.admin.head')
+
+<!-- body start -->
+
+<body class="loading"
+    data-layout='{"mode": "light", "width": "fluid", "menuPosition": "fixed", "sidebar": { "color": "light", "size": "default", "showuser": false}, "topbar": {"color": "dark"}, "showRightSidebarOnPageLoad": true}'>
+
+    <!-- Begin page -->
+    <div id="wrapper">
+
+        <!-- Topbar Start -->
+        <div class="navbar-custom">
+            <div class="container-fluid">
+                <ul class="list-unstyled topnav-menu float-end mb-0">
+
+
+                    <li class="dropdown notification-list topbar-dropdown">
+                        <a class="nav-link dropdown-toggle nav-user me-0 waves-effect waves-light"
+                            data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false"
+                            aria-expanded="false">
+                            <span class="pro-user-name ms-1">
+                                Admin <i class="mdi mdi-chevron-down"></i>
+                            </span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end profile-dropdown ">
+                            <!-- item-->
+                            <div class="dropdown-header noti-title">
+                                <h6 class="text-overflow m-0">Welcome !</h6>
+                            </div>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="fe-user"></i>
+                                <span>My Account</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="fe-settings"></i>
+                                <span>Settings</span>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="fe-lock"></i>
+                                <span>Lock Screen</span>
+                            </a>
+
+                            <div class="dropdown-divider"></div>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <i class="fe-log-out"></i>
+                                <span>Logout</span>
+                            </a>
+
+                        </div>
+                    </li>
+
+                </ul>
+
+                <!-- LOGO -->
+                <div class="logo-box">
+                    <a href="index.html" class="logo logo-dark text-center">
+                        <span class="logo-sm">
+                            <img src="../assets/images/logo-sm.png" alt="" height="22">
+                            <!-- <span class="logo-lg-text-light">UBold</span> -->
+                        </span>
+                        <span class="logo-lg">
+                            <img src="../assets/images/logo-dark.png" alt="" height="20">
+                            <!-- <span class="logo-lg-text-light">U</span> -->
+                        </span>
+                    </a>
+
+                    <a href="index.html" class="logo logo-light text-center">
+                        <span class="logo-sm">
+                            <img src="../assets/images/logo-sm.png" alt="" height="22">
+                        </span>
+                        <span class="logo-lg">
+                            <img src="../assets/images/logo-light.png" alt="" height="20">
+                        </span>
+                    </a>
+                </div>
+
+                <ul class="list-unstyled topnav-menu topnav-menu-left m-0">
+                    <li>
+                        <button class="button-menu-mobile waves-effect waves-light">
+                            <i class="fe-menu"></i>
+                        </button>
+                    </li>
+
+                    <li>
+                        <!-- Mobile menu toggle (Horizontal Layout)-->
+                        <a class="navbar-toggle nav-link" data-bs-toggle="collapse"
+                            data-bs-target="#topnav-menu-content">
+                            <div class="lines">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </a>
+                        <!-- End mobile menu toggle-->
+                    </li>
+
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <!-- end Topbar -->
+
+        @include('layouts.admin.sidebar')
+
+        <!-- ============================================================== -->
+        <!-- Start Page Content here -->
+        <!-- ============================================================== -->
+
+        <div class="content-page">
+            <div class="content">
+
+                <!-- Start Content-->
+                <div class="container-fluid">
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box">
+
+                                <h4 class="page-title">Events</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row justify-content-between mb-2">
+                                        <div class="col-auto">
+                                            <form>
+                                                <div class="mb-2">
+                                                    <label for="inputPassword2" class="visually-hidden">Search</label>
+                                                    <input type="search" class="form-control" id="inputPassword2"
+                                                        placeholder="Search...">
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="text-sm-end">
+                                                {{-- <button type="button" class="btn btn-danger waves-effect waves-light mb-2" data-bs-toggle="modal" data-bs-target="#custom-modal">Add Contact</button> --}}
+                                                <Span class="bg-info p-2" style="color:#fff" data-bs-toggle="modal"
+                                                    data-bs-target="#custom-modal-events"><a href="#"
+                                                        data-bs-toggle="modal" data-bs-target="#custom-modal-events"
+                                                        style="color:#fff">Add Events</a> </Span>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="table-responsive">
+                                        <table class="table table-centered table-nowrap table-hover mb-0">
+                                            <thead>
+                                                <tr>
+                                                    <th>sn</th>
+                                                    <th>title</th>
+                                                    <th>Image</th>
+                                                    <th>Discription</th>
+                                                    <th>Location</th>
+                                                    <th>Start Date</th>
+                                                    <th>End Date</th>
+                                                    <th style="width: 82px;">Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td class="table-user">
+                                                        1
+                                                    </td>
+                                                    <td>
+                                                        Vine Corporation
+                                                    </td>
+                                                    <td>
+                                                        
+                                                        <img src="../assets/images/users/user-4.jpg" alt="table-user"
+                                                            class="me-2 rounded-circle">
+                                                    </td>
+                                                    <td>
+                                                        Uzinduzi wa kigango cha Mt. Francis Ifakara.
+                                                    </td>
+                                                    <td>
+                                                        Ifakara.
+                                                    </td>
+                                                    <td>
+                                                        Tar: 12-4-2023
+                                                    </td>
+                                                    <td>
+                                                        Tar: 16-4-2023
+                                                    </td>
+                                                    <td>
+                                                        <a href="javascript:void(0);" class="action-icon"> <i
+                                                                class="mdi mdi-square-edit-outline"></i></a>
+                                                        <a href="javascript:void(0);" class="action-icon"> <i
+                                                                class="mdi mdi-delete"></i></a>
+                                                    </td>
+                                                </tr>
+
+
+
+
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                    <ul class="pagination pagination-rounded justify-content-end mb-0 mt-2">
+                                        <li class="page-item">
+                                            <a class="page-link" href="javascript: void(0);" aria-label="Previous">
+                                                <span aria-hidden="true">«</span>
+                                                <span class="visually-hidden">Previous</span>
+                                            </a>
+                                        </li>
+                                        <li class="page-item active"><a class="page-link"
+                                                href="javascript: void(0);">1</a></li>
+                                        <li class="page-item"><a class="page-link" href="javascript: void(0);">2</a>
+                                        </li>
+                                        <li class="page-item"><a class="page-link" href="javascript: void(0);">3</a>
+                                        </li>
+                                        <li class="page-item"><a class="page-link" href="javascript: void(0);">4</a>
+                                        </li>
+                                        <li class="page-item"><a class="page-link" href="javascript: void(0);">5</a>
+                                        </li>
+                                        <li class="page-item">
+                                            <a class="page-link" href="javascript: void(0);" aria-label="Next">
+                                                <span aria-hidden="true">»</span>
+                                                <span class="visually-hidden">Next</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+
+                                </div>
+                                <!-- end card-body-->
+                            </div>
+                            <!-- end card-->
+                        </div>
+                        <!-- end col -->
+
+                    </div>
+                    <!-- end row -->
+
+                </div>
+                <!-- container -->
+
+            </div>
+            <!-- content -->
+            <!-- Modal -->
+            <div class="modal fade" id="custom-modal-events" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header bg-light">
+                            <h4 class="modal-title" id="myCenterModalLabel">Add Events</h4>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body p-4">
+                            <form>
+                                <div class="row">
+                                    <div class="mb-3 col-12">
+                                        <label for="name" class="form-label">Title</label>
+                                        <input type="text" class="form-control" id="name"
+                                            placeholder="Enter Event Title">
+                                    </div>
+                                    <div class="mb-3 col-12">
+                                        <label for="exampleInputEmail1" class="form-label">Image</label>
+                                        <input type="file" class="form-control" id="exampleInputEmail1"
+                                            placeholder="Enter email">
+                                    </div>
+                                    <div class="mb-3 col-6">
+                                        <label for="name" class="form-label">Guest</label>
+                                        <input type="text" class="form-control" id="name"
+                                            placeholder="Enter Guest Name">
+                                    </div>
+                                    <div class="mb-3 col-6">
+                                        <label for="name" class="form-label">Location</label>
+                                        <input type="text" class="form-control" id="name"
+                                            placeholder="Enter Event Location">
+                                    </div>
+                                    <div class="mb-3 col-12">
+                                        <label for="position" class="form-label">Description</label>
+                                        <textarea name="Description" id="" cols="12" rows="4" class="form-control"></textarea>
+                                    </div>
+    
+                                    <div class="text-end">
+                                        <button type="submit" class="bg-info p-2">Publish </button>
+                                    </div>
+                                </div>
+                                
+                            </form>
+                        </div>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
+            <!-- /.modal -->
+
+            <!-- Footer Start -->
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <b>IFAKARA DIOCESE Ⓒ 2023</b>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- end Footer -->
+
+        </div>
+
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+
+
+    </div>
+
+    <script src="{{ asset('admin/assets/js/vendor.min.js') }}"></script>
+
+
+    <script src="{{ asset('admin/assets/js/pages/crm-dashboard.init.js') }}"></script>
+
+    <!-- App js -->
+    <script src="{{ asset('admin/assets/js/app.min.js') }}"></script>
+
+</body>
+
 </html>
