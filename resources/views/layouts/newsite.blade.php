ed59ff6d6f6c3c6fa62743c5be861f6074f63849
adevotus <adevoweb@gmail.com>
2023-03-12 12:28:31 +0300

    event tabs

--- resources/views/layouts/newsite.blade.php
+++ resources/views/layouts/newsite.blade.php
@@ -4,27 +4,26 @@
             <!-- Intro News Tabs Area -->
             <div class="col-12 col-lg-8">
                 <div class="intro-news-tab">   
-                    <!-- Intro News Filter -->
-                    <div class="intro-news-filter d-flex justify-content-between"style="background-color:#ffff; color:#ffff; box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;">
-                        <h6 s style="color:#ff7b00; font-weight:700; text-align:center; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">All News </h6>
-                        <nav style="background-color: #000">
-                            <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                                <a class="nav-item nav-link active" id="nav1" data-toggle="tab"
-                                    href="#nav-1" role="tab" aria-controls="nav-1"
-                                    aria-selected="true" style="color:#000">Events</a>
 
+                       <!-- Intro News Filter -->
+                       <div class="intro-news-filter d-flex justify-content-between" style="background-color:#ffff; color:#ffff; box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;">
+                        <h6 style="color:#ff7b00; font-weight:700; text-align:center; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;">All the news</h6>
+                        <nav>
+                            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+
+                                <a class="nav-item nav-link active" id="nav2" data-toggle="tab" href="#nav-2" role="tab" aria-controls="nav-2" aria-selected="false" style="color:#000">News</a>
+                                <a class="nav-item nav-link" id="nav4" data-toggle="tab" href="#nav-4" role="tab" aria-controls="nav-4" aria-selected="false" style="color:#000">Events</a>
 
                             </div>
                         </nav>
                     </div>
 
+
                     <div class="tab-content" id="nav-tabContent">
 
-                        <div class="tab-pane fade show active" id="nav-1" role="tabpanel"
-                            aria-labelledby="nav1">
+                       
+                        <div class="tab-pane fade" id="nav-2" role="tabpanel" aria-labelledby="nav2">
                             <div class="row">
-                                <!-- Single News Area -->
-                                <!-- Single News Area -->
                                 @foreach ($news as $news)
                                 <div class="col-12 col-sm-6">
                                     <div class="single-blog-post d-flex style-4 mb-30">
@@ -43,13 +42,103 @@
                                     </div>
                                 </div>
                                 @endforeach
+                                
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="nav-4" role="tabpanel" aria-labelledby="nav4">
+                            <div class="row">
+                                <!-- Single News Area -->
+                                <div class="col-12">
+                                    <div class="single-blog-post style-2 mb-5">
+                                        <!-- Blog Thumbnail -->
+                                        <div class="blog-thumbnail">
+                                            <a href="#"><img src="img/bg-img/27.jpg" alt=""></a>
+                                        </div>
+
+                                        <!-- Blog Content -->
+                                        <div class="blog-content">
+                                            <span class="post-date">June 20, 2018</span>
+                                            <a href="#" class="post-title">Elon Musk: Tesla worker admitted to sabotage</a>
+                                            <a href="#" class="post-author mb-30">By Michael Smith</a>
+                                            <p>Nullam lacinia ex eleifend orci porttitor, suscipit interdum augue condimentum. Etiam pretium turpis eget nibh laoreet iaculis. Proin ac urna at lectus volutpat lobortis. Vestibulum venenatis iaculis diam vitae lobortis. Donec tincidunt viverra elit, sed consectetur est pr etium ac. Mauris nec mauris tellus. </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Single News Area -->
+                                <div class="col-12 col-sm-6">
+                                    <div class="single-blog-post style-2 mb-5">
+                                        <!-- Blog Thumbnail -->
+                                        <div class="blog-thumbnail">
+                                            <a href="#"><img src="img/bg-img/14.jpg" alt=""></a>
+                                        </div>
+
+                                        <!-- Blog Content -->
+                                        <div class="blog-content">
+                                            <span class="post-date">June 20, 2018</span>
+                                            <a href="#" class="post-title">Elon Musk: Tesla worker admitted to sabotage</a>
+                                            <a href="#" class="post-author">By Michael Smith</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Single News Area -->
+                                <div class="col-12 col-sm-6">
+                                    <div class="single-blog-post style-2 mb-5">
+                                        <!-- Blog Thumbnail -->
+                                        <div class="blog-thumbnail">
+                                            <a href="#"><img src="img/bg-img/15.jpg" alt=""></a>
+                                        </div>
+
+                                        <!-- Blog Content -->
+                                        <div class="blog-content">
+                                            <span class="post-date">June 20, 2018</span>
+                                            <a href="#" class="post-title">Rachel Sm ith breaks down while discussing border crisis</a>
+                                            <a href="#" class="post-author">By Michael Smith</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Single News Area -->
+                                <div class="col-12 col-sm-6">
+                                    <div class="single-blog-post style-2 mb-5">
+                                        <!-- Blog Thumbnail -->
+                                        <div class="blog-thumbnail">
+                                            <a href="#"><img src="img/bg-img/4.jpg" alt=""></a>
+                                        </div>
+
+                                        <!-- Blog Content -->
+                                        <div class="blog-content">
+                                            <span class="post-date">June 20, 2018</span>
+                                            <a href="#" class="post-title">Elon Musk: Tesla worker admitted to sabotage</a>
+                                            <a href="#" class="post-author">By Michael Smith</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Single News Area -->
+                                <div class="col-12 col-sm-6">
+                                    <div class="single-blog-post style-2 mb-5">
+                                        <!-- Blog Thumbnail -->
+                                        <div class="blog-thumbnail">
+                                            <a href="#"><img src="img/bg-img/5.jpg" alt=""></a>
+                                        </div>
+
+                                        <!-- Blog Content -->
+                                        <div class="blog-content">
+                                            <span class="post-date">June 20, 2018</span>
+                                            <a href="#" class="post-title">Rachel Sm ith breaks down while discussing border crisis</a>
+                                            <a href="#" class="post-author">By Michael Smith</a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <!-- Sidebar Widget -->
+            <!-- Sidebar gallery -->
             <div class="col-12 col-sm-9 col-md-6 col-lg-4">
                 <div class="sidebar-area">
                     <div class="intro-news-filter d-flex justify-content-between" style="background-color:#fff; color:#ffff;  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;">
